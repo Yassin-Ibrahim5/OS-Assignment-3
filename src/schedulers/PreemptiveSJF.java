923111efc86c58437f540f6a74d0210492618396
ZiyadHawary <154548226+ZiyadHawary@users.noreply.github.com>
2025-12-17 16:57:38 +0200

    finished SJF

--- src/schedulers/PreemptiveSJF.java
+++ src/schedulers/PreemptiveSJF.java
@@ -11,8 +11,63 @@ public class PreemptiveSJF extends SchedulerBase {
 
     @Override
     public void schedule() {
-        System.out.println("Running Preemptive SJF...");
-        // TODO: Implement SJF algorithm here
-        // Remember to call calculateTimes() at the end
+        System.out.println("Running Preemptive SJF ...");
+
+        int currentTime = 0;
+        int completedProcesses = 0;
+        Process currentProcess = null;
+
+        PriorityQueue<Process> readyQueue = new PriorityQueue<>(
+                Comparator.comparingInt(Process::getRemainingTime)
+                        .thenComparingInt(Process::getArrivalTime)
+        );
+
+        Set<Process> arrivedProcesses = new HashSet<>();
+
+        while (completedProcesses < processes.size()) {
+            for (Process p : processes) {
+                if (p.getArrivalTime() <= currentTime && !arrivedProcesses.contains(p)) {
+                    readyQueue.add(p);
+                    arrivedProcesses.add(p);
+                }
+            }
+
+            // 2. Check for Preemption (if a new process is shorter than current)
+            if (currentProcess != null) {
+                Process shortest = readyQueue.peek();
+                // If there is a process in queue shorter than current process
+                if (shortest != null && shortest.getRemainingTime() < currentProcess.getRemainingTime()) {
+                    // Context Switch Overhead
+                    currentTime += contextSwitchTime;
+                    readyQueue.add(currentProcess);
+                    currentProcess = null;
+                    continue;
+                }
+            }
+
+            // Pick Process if CPU is idle
+            if (currentProcess == null && !readyQueue.isEmpty()) {
+                currentProcess = readyQueue.poll();
+            }
+
+            if (currentProcess != null) {
+                currentProcess.setRemainingTime(currentProcess.getRemainingTime() - 1);
+                currentTime++;
+
+                if (currentProcess.isComplete()) {
+                    currentProcess.setCompletionTime(currentTime);
+                    completedProcesses++;
+                    currentProcess = null;
+
+                    if (completedProcesses < processes.size()) {
+                        currentTime += contextSwitchTime;
+                    }
+                }
+            } else {
+//                pass time if no process is ready
+                currentTime++;
+            }
+        }
+        calculateTimes();
     }
 }
